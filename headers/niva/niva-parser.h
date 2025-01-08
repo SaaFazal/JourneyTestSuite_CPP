90d7854c4880ad6619e38cc8f8a136816008fbd5
Neil Sculthorpe <neil.sculthorpe@ntu.ac.uk>
2025-01-08 16:05:13 +0000

    Adding Commits

--- headers/niva/niva-parser.h
+++ headers/niva/niva-parser.h
@@ -41,51 +41,11 @@ bool hasCorrectNumberOfFields(const NIVA::DataReading& d);
 //    - the data reading has the correct number of fields.
 GPS::Waypoint extractWaypointFromReading(const NIVA::DataReading& d);
 
-<<<<<<< HEAD
 // Extracts the GPS location data from a stream of raw text NIVA readings.
 // The stream is assumed to be a sequence of NIVA data readings, optionally separated by whitespace.
 // Any text in the stream that is not a well-formed data reading is skipped.
 // Data readings with erroneous values in the fields are also skipped.
 std::vector<GPS::Waypoint> extractWaypointsFromLog(std::istream& is);
-=======
-  // Compute the XOR reduction of all of the ASCII character codes in the provided raw text.
-  unsigned int computeChecksum(std::string);
-
-
-
-  // Check whether the (decimal notation) checksum value in the provided raw text of a NIVA data reading matches
-  // the checksum computed from the raw text between the square brackets.
-  // Pre-condition: the provided string is a well-formed NIVA data reading.
-  bool hasMatchingChecksum(std::string);
-
-
-
-  // Parses the raw text of a NIVA data reading and stores the contents in a structured form in a NIVA::DataReading.
-  // Pre-condition: the provided string is a well-formed NIVA data reading.
-  NIVA::DataReading parseDataReading(std::string);
-
-
-
-  // Checks whether the data reading contains the correct number of fields for the reading type of the format code.
-  // Pre-condition: the reading contains a known format code in block capitals.
-  bool hasCorrectNumberOfFields(NIVA::DataReading);
-
-
-
-  // Extracts the GPS location data from the data reading and constructs a Waypoint object.
-  // Pre-conditions:
-  //    - the data reading contains a known format code in block capitals.
-  //    - the data reading has the correct number of fields.
-  GPS::Waypoint extractWaypointFromReading(NIVA::DataReading);
-
-
-
-  // Extracts the GPS location data from a stream of raw text NIVA readings.
-  // The stream is assummed to be a sequence of NIVA data readings, optionally separated by whitespace.
-  // Any text in the stream that is not a well-formed data reading is skipped.
-  // Data readings with erroneus values in the fields are also skipped.
-  std::vector<GPS::Waypoint> extractWaypointsFromLog(std::istream&);
->>>>>>> 08a765c (Changed NIVA checksum delimiter symbols)
 }
 
 #endif
