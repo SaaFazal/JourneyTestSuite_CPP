62c16a78d1180e2daa64d23d05aa25c4c5e10dff
SaadFazal <sahadh.fazal@gmail.com>
2025-03-04 17:16:46 +0000

    HasMatchingChecksum is Updated

--- src/niva/niva-parser.cpp
+++ src/niva/niva-parser.cpp
@@ -129,7 +129,15 @@ namespace NIVA
     unsigned int computedChecksum = computeChecksum(data);
 
     // Convert the provided checksum to an integer
-    unsigned int providedChecksum = std::stoul(checksumStr);
+    unsigned int providedChecksum;
+    try
+    {
+        providedChecksum = std::stoul(checksumStr);
+    }
+    catch (const std::invalid_argument&)
+    {
+        return false; // Invalid checksum format
+    }
 
     // Compare the computed checksum with the provided checksum
     return computedChecksum == providedChecksum;
